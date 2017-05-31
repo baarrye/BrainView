f6dd1594bec10b7a8e3d2fa9f545a6de7e2513fe
Davide <barranca.davide98@gmail.com>
2017-05-31 16:46:11 +0200

    Mod

---
+++ BrainView/Assets/Script/Grafici/AttMed/LuceAttenzione.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LuceAttenzione : MonoBehaviour {
+
+	public DisplayData controller;
+
+	private int attention;
+
+	public Light luceAtt;
+
+	void Start () {
+		
+	}
+
+
+	void OnUpdateAttention(int value){
+		attention = value;
+	}
+		
+	void update(int valueAtt, int valueMed){
+		//=controller.
+		//valueAtt = 1 * attention / 100;
+		//luceAtt.intensity = (float)valueAtt;
+	}
+
+}
---
+++ BrainView/Assets/Script/Grafici/AttMed/LuceMeditazione.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LuceMeditazione : MonoBehaviour {
+
+	TGCConnectionController controller;
+
+	private int meditation;
+
+	public Light luceMed;
+
+	void Start () {
+		controller = GameObject.Find("NeuroSkyTGCController").GetComponent<TGCConnectionController>();
+		controller.UpdateMeditationEvent += OnUpdateMeditation;
+	}
+
+
+
+	void OnUpdateMeditation(int value){
+		meditation = value;
+	}
+
+	void update(int valueMed){
+		valueMed = 1 * meditation / 100;
+
+		luceMed.intensity = (float)valueMed;
+	}
+
+}
